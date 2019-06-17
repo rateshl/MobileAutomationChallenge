e5509ba94f76c9c54100c07a7403685ad14ce705
rateshl <ratesh.cc@gmail.com>
2019-06-17 12:11:21 +0530

    Commit

--- MobileAutomationChallenge/src/test/java/com/brabbler/reusables/Reusables.java
+++ MobileAutomationChallenge/src/test/java/com/brabbler/reusables/Reusables.java
@@ -3,6 +3,7 @@ package com.brabbler.reusables;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidElement;
 
+//Reusables will be written in this package
 public class Reusables {
 	
 	public void reusable1(){
